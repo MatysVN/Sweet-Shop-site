12b221e061133b3c8195221bbb3c631fb161c12e
Mateusz <mateuszskc@gmail.com>
2024-02-24 14:56:32 +0100

    finished header and nav

--- js/style.js
+++ js/style.js
@@ -1,7 +1,9 @@
 const burgerBtn = document.querySelector('.hamburger');
+const navMobile = document.querySelector('.nav');
 
 const showNav = () => {
-    burgerBtn.classList.toggle("is-active")
+    burgerBtn.classList.toggle("is-active");
+    navMobile.classList.toggle("nav__mobile-active")
 }
 
 burgerBtn.addEventListener("click", showNav);
