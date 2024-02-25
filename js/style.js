d7c0a5d48c71e0d551ada2d95ae33fdaff04e630
Mateusz <mateuszskc@gmail.com>
2024-02-25 08:27:07 +0100

    Navigation has been optimized

--- js/style.js
+++ js/style.js
@@ -1,9 +1,11 @@
 const burgerBtn = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.nav');
+const scrollBlock = document.querySelector('body');
 
 const showNav = () => {
     burgerBtn.classList.toggle("is-active");
-    navMobile.classList.toggle("nav__mobile-active")
+    navMobile.classList.toggle("nav__mobile-active");
+    scrollBlock.classList.toggle("scroll-block");
 }
 
 burgerBtn.addEventListener("click", showNav);
