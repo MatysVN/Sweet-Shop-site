3048dcba97db6abc6c2bba24516a05bb276829d5
Mateusz <mateuszskc@gmail.com>
2024-03-01 23:13:05 +0100

    before experiment with slick

---
+++ js/slick.js
@@ -0,0 +1,23 @@
+$(".team-carousel").slick({
+	arrows: false,
+	autoplay: true,
+    autoplaySpeed: 2000,
+    swipe: true,
+	mobileFirst: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+        {
+			breakpoint: 991,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+	]
+});
