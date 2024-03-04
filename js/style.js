bdb45d5a3a4e1c003b50cc57ecbb81f23ff844fd
Mateusz <mateuszskc@gmail.com>
2024-03-04 21:52:53 +0100

    create footer

--- js/style.js
+++ js/style.js
@@ -1,11 +1,19 @@
 const burgerBtn = document.querySelector('.hamburger');
 const navMobile = document.querySelector('.nav');
 const scrollBlock = document.querySelector('body');
+const footerYear = document.querySelector('.footer-year')
 
 const showNav = () => {
     burgerBtn.classList.toggle("is-active");
     navMobile.classList.toggle("nav__mobile-active");
     scrollBlock.classList.toggle("scroll-block");
 }
+
+const currentYear = () => {
+    const year = new Date().getFullYear();
+    footerYear.innerText = year;
+}
+
+currentYear();
 
 burgerBtn.addEventListener("click", showNav);
