d249848054323fa648c0f6e07fd4a37963e30a64
Mateusz <mateuszskc@gmail.com>
2024-03-05 18:13:08 +0100

    finish main site

--- js/style.js
+++ js/style.js
@@ -1,19 +1,55 @@
-const burgerBtn = document.querySelector('.hamburger');
-const navMobile = document.querySelector('.nav');
-const scrollBlock = document.querySelector('body');
-const footerYear = document.querySelector('.footer-year')
+const burgerBtn = document.querySelector(".hamburger");
+const burgerBox = document.querySelector(".hamburger-box");
+const navMobile = document.querySelector(".nav");
+const navMobileItem = document.querySelectorAll(".nav__item");
+const scrollBlock = document.querySelector("body");
+const footerYear = document.querySelector(".footer-year");
+const headerSection = document.querySelector(".header");
+const headerArrow = document.querySelector(".header__arrow");
 
 const showNav = () => {
-    burgerBtn.classList.toggle("is-active");
-    navMobile.classList.toggle("nav__mobile-active");
-    scrollBlock.classList.toggle("scroll-block");
-}
+	burgerBtn.classList.toggle("is-active");
+	navMobile.classList.toggle("nav__mobile-active");
+	scrollBlock.classList.toggle("scroll-block");
+};
+
+window.onscroll = function () {
+	const sectionBottom = headerSection.offsetTop + headerSection.offsetHeight;
+
+	if (window.pageYOffset > sectionBottom) {
+		navMobile.classList.remove("hiddenElement");
+	} else {
+		navMobile.classList.add("hiddenElement");
+	}
+	if (window.pageYOffset > sectionBottom) {
+		burgerBox.classList.remove("hiddenElement");
+	} else {
+		burgerBox.classList.add("hiddenElement");
+	}
+};
+headerArrow.addEventListener("click", function (event) {
+	event.preventDefault();
+	const targetId = headerArrow.getAttribute("href");
+	const targetSection = document.querySelector(targetId);
+	const scrollTo = targetSection.offsetTop - 40;
+	window.scrollTo({
+		top: scrollTo,
+		behavior: "smooth", // Dodaje płynne przewijanie
+	});
+});
 
 const currentYear = () => {
-    const year = new Date().getFullYear();
-    footerYear.innerText = year;
-}
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
 
 currentYear();
 
 burgerBtn.addEventListener("click", showNav);
+navMobileItem.forEach(function (item) {
+	item.addEventListener("click", closeNav);
+});
+
+window.addEventListener('load', function() {
+    window.dispatchEvent(new Event('scroll'));
+});
